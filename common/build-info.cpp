52b7069c3e566e03a3bbf3dc50f76fb56e9545a6
yjzhang0505 <zzyyjj0505@163.com>
2025-01-18 16:50:13 +0800

    delete

--- common/build-info.cpp
+++
@@ -1,4 +0,0 @@
-int LLAMA_BUILD_NUMBER = 2612;
-char const *LLAMA_COMMIT = "1b496a74";
-char const *LLAMA_COMPILER = "cc (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0";
-char const *LLAMA_BUILD_TARGET = "x86_64-linux-gnu";
